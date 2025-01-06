7d7af6dbcb38b6d06ede96bda253c72ac417bb24
miz2o <mizzosaur@Gmail.com>
2025-01-06 16:16:40 +0100

    devtools

---
+++ Assets/Scripts/DevTools.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevTools : MonoBehaviour
+{
+    public GameObject cashHandlerParent;
+
+    // List of objects to spawn
+    public List<GameObject> objectsToSpawn;
+
+    // Position where the objects will spawn
+    public Vector3 spawnPosition;
+
+    // Offset between spawned objects
+    public Vector3 spawnOffset;
+
+    // Method to add cash
+    public void TryBuy()
+    {
+        print("Devtools started");
+        CashHandler cashHandler = cashHandlerParent.GetComponent<CashHandler>();
+        if (cashHandler != null)
+        {
+            cashHandler.cash += 999999;
+            SpawnObjects();
+        }
+    }
+
+    // Method to spawn all objects
+    public void SpawnObjects()
+    {
+        for (int i = 0; i < objectsToSpawn.Count; i++)
+        {
+            // Check if the object in the list is valid
+            if (objectsToSpawn[i] != null)
+            {
+                // Calculate the position for this object with the offset
+                Vector3 currentSpawnPosition = spawnPosition + (spawnOffset * i);
+
+                // Spawn the object at the calculated position
+                Instantiate(objectsToSpawn[i], currentSpawnPosition, Quaternion.identity);
+            }
+        }
+    }
+}
