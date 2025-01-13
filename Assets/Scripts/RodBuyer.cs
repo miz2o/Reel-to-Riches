fe63306fcab5a92e69c2e1fd1bc8adf176b75347
miz2o <mizzosaur@Gmail.com>
2025-01-13 15:38:15 +0100

    rodbuy fix

--- Assets/Scripts/RodBuyer.cs
+++ Assets/Scripts/RodBuyer.cs
@@ -25,23 +25,7 @@ public class RodBuyer : MonoBehaviour
                 cashHandler.cash -= price;
                 buytext.text = "Bought";
 
-                // Check if rodToBuy and storeSpawn are set
-                if (rodToBuy != null && storeSpawn != null)
-                {
-                    // Instantiate the rodToBuy at the storeSpawn location and rotation
-                    GameObject newRod = Instantiate(rodToBuy, storeSpawn.transform.position, storeSpawn.transform.rotation);
-
-                    // Check if the new rod has a Rigidbody and set isKinematic to false
-                    Rigidbody rodRigidbody = newRod.GetComponent<Rigidbody>();
-                    if (rodRigidbody != null)
-                    {
-                        rodRigidbody.isKinematic = false;
-                    }
-                }
-                else
-                {
-                    Debug.LogWarning("Rod or Spawn location is not set!");
-                }
+      rodToBuy.transform.position = storeSpawn.transform.position;
             }
             else
             {
